bc6a9ad3e5126d90c944e7004cdce63fca46e64e
esonboyev03@bk.ru <esonboyev03@gmail.com>
2021-09-21 16:22:36 +0000

    lesson3

---
+++ homework1.cpp
@@ -0,0 +1,16 @@
+
+#include <stdio.h> 
+
+
+int main (){
+	int x = 2;
+	int y = 3;
+	
+	printf(" x = %d \n y = %d \n", x , y);
+	scanf("%d");
+	printf("x = 3 \n", y);
+	printf("y = 2", x);
+	return 0;
+}
+
+
